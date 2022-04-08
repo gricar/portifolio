7eb9026be818e527c1426e8e3182106a5c3fcb8d
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-04-08 20:05:27 -0300

    created header component and basic pallet colors

---
+++ src/pages/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Sobre mim</li>
+          <li>Projetos</li>
+          <li>Contato</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
